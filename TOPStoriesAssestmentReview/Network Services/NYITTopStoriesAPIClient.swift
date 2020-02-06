4cb9016ae6961bffddf24ebcbf827185bde76e46
melinda <melindad_82@msn.com>
2020-02-06 12:52:18 -0500

    added APIClient

---
+++ TOPStoriesAssestmentReview/Network Services/NYITTopStoriesAPIClient.swift
@@ -0,0 +1,36 @@
+//
+//  NYITTopStoriesAPIClient.swift
+//  TOPStoriesAssestmentReview
+//
+//  Created by Melinda Diaz on 2/6/20.
+//  Copyright © 2020 Melinda Diaz. All rights reserved.
+//
+
+import Foundation
+import NetworkHelper
+
+
+struct NYTTopStoriesAPIClient {
+    static func fetchTopStories(for section: String, completion: @escaping (Result<[Article], AppError>) -> ()) {
+        let endpointURLString = "https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key="
+        guard let url = URL(string: endpointURLString) else {
+            completion(.failure(.badURL(endpointURLString)))
+            return
+        }
+        let request = URLRequest(url: url)
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let topStories = try JSONDecoder().decode(TopStories.self, from: data)
+                    completion(.success(topStories.results))
+                } catch {
+                    //here is where you catch the error
+                    completion(.failure(.decodingError(error)))
+                }
+        }
+    }
+}
+}
